731bc09cec4c263ce562eeb085188586bb4f0f9f
saul roldan <saul28roldan@gmail.com>
2025-08-31 00:20:04 -0300

    castor ready
    agrego el metodo tocar guitarra

--- ej5integrador/src/ej5/Castor.java
+++ ej5integrador/src/ej5/Castor.java
@@ -1,11 +1,13 @@
 
 package ej5;
 
-public class Castor {
-    private int cola;
-    private int velocidad;      // (hasta 8km/h. ingresar por teclado)
+import java.util.Random;
 
-    public Castor(int cola, int velocidad) {
+public  class Castor {
+    private Double cola;
+    private Double velocidad;      // (hasta 8km/h. ingresar por teclado)
+
+    public Castor(Double cola, Double velocidad) {
         this.cola = cola;
         this.velocidad = velocidad;
     }
@@ -13,10 +15,18 @@ public class Castor {
     
     
     
-    public int nadar(){
-        return velocidad;
+    public void nadar(){
+        
+        System.out.println(" nada a una velocidad de " +velocidad+ " km/hs");
 
     }    
-    public void tocarGuitarra(){
+    public  void tocarGuitarra(){
+    Random r= new Random(6);
+    for(int i=0;i<3;i++){
+        Random cuerda=r;
+        System.out.println("toca cuerda "+cuerda);
+        
     }
+    }
+    
 }
